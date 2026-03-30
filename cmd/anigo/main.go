5616eff58e51124d7e120949fadda342ee23cc8d
PinguinAdvokat <you@example.com>
2026-03-30 16:42:47 +0300

    fix

---
+++ cmd/anigo/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"anigo/internal/app"
+	"anigo/internal/cache"
+	"anigo/internal/extractors/animego"
+	"anigo/internal/initApp"
+	"anigo/internal/manager"
+	"anigo/internal/parsers/kodik"
+	"flag"
+	"io"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	verbose := flag.Bool("v", false, "включить отладочные логи")
+	flag.Parse()
+
+	if !*verbose {
+		log.SetOutput(io.Discard)
+	}
+
+	appDir := initApp.Init()
+	cache := cache.New(appDir)
+	httpClient := &http.Client{Timeout: 5 * time.Second}
+	kodikParser := kodik.New(httpClient, cache)
+	animego := animego.New(httpClient)
+	manager := manager.New(animego, kodikParser)
+
+	app := app.New(manager)
+	if err := app.EnableMouse(true).EnablePaste(true).Run(); err != nil {
+		panic(err)
+	}
+}
